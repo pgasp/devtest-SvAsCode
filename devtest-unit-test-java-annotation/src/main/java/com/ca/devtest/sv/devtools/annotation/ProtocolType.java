b5f469d2a961ea8d23ffd685367b5ad37b7100a2
gaspa03 <gaspa03@ca.com>
2017-03-16 15:40:25 +0100

    initial commit

---
+++ devtest-unit-test-java-annotation/src/main/java/com/ca/devtest/sv/devtools/annotation/ProtocolType.java
@@ -0,0 +1,14 @@
+package com.ca.devtest.sv.devtools.annotation;
+
+/**
+ * @author gaspa03
+ *
+ */
+public interface ProtocolType {
+
+	public String DPH_DOIT= "com.personalfinance.devtest.dph.DoItDataHandler";
+	public String DPH_SOAP = "com.itko.lisa.vse.stateful.protocol.ws.WSSOAPProtocolHandler";
+	public String TPH_HTTP = "com.itko.lisa.vse.stateful.protocol.http.HttpProtocolHandler";
+	public String TPH_SWEETDEV="com.bnpparibas.devtest.extension.protocol.sweetdev.SweetDevRRTransportProtocol";
+	
+}
